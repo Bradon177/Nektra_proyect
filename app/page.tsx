92a7763f0c1424355d40a08f3a7fa3ef502fe0f1
Bradon177 <trillosbrandon262@gmail.com>
2025-11-14 08:14:30 -0500

    Arreglo de nombres

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+
+  const routrer = useRouter()
+
+  routrer.push("/")
+
+
+
+
+
+
+
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      
